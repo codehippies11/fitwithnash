cbe2d305505e4b3e1156adcf0ff94a195a1b26b6
codehippies11 <codehippies11@gmail.com>
2026-08-15 11:51:20 +0530

    Strengthen sitewide technical SEO

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,12 @@ export const onRequest = defineMiddleware((context, next) => {
   if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/') {
     return context.rewrite('/consult');
   }
+  if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/sitemap.xml') {
+    return context.rewrite('/consult-sitemap.xml');
+  }
+  if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/robots.txt') {
+    return context.rewrite('/consult-robots.txt');
+  }
 
   return next();
 });
