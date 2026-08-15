f40c0d1eca16538cbfb8695b75fca6536788c7e3
codehippies11 <codehippies11@gmail.com>
2026-08-15 11:51:48 +0530

    Add consultation crawl endpoints

---
+++ src/pages/consult-sitemap.xml.ts
@@ -0,0 +1,3 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = () => new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://consult.fitwithnash.com/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url></urlset>`, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
