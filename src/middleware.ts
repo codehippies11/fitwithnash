b3ed0f7e8ac6404f5534fc657d208996c7968ed4
codehippies11 <codehippies11@gmail.com>
2026-08-23 02:16:10 +0530

    Redirect primary domains to consult funnel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
 import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
-  if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/') {
+  const hostname = context.url.hostname.toLowerCase();
+
+  if (hostname === 'fitwithnash.com' || hostname === 'www.fitwithnash.com') {
+    return Response.redirect(
+      new URL(`${context.url.pathname}${context.url.search}`, 'https://consult.fitwithnash.com'),
+      308,
+    );
+  }
+
+  if (hostname === 'consult.fitwithnash.com' && context.url.pathname === '/') {
     return context.rewrite('/consult');
   }
-  if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/sitemap.xml') {
+  if (hostname === 'consult.fitwithnash.com' && context.url.pathname === '/sitemap.xml') {
     return context.rewrite('/consult-sitemap.xml');
   }
-  if (context.url.hostname === 'consult.fitwithnash.com' && context.url.pathname === '/robots.txt') {
+  if (hostname === 'consult.fitwithnash.com' && context.url.pathname === '/robots.txt') {
     return context.rewrite('/consult-robots.txt');
   }
 
